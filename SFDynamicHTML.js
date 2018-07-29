754c872ffab17c2ff4e0381bafa0fa0e8a82a074
StefansArya <arya.cipta@yahoo.com>
2018-07-29 17:58:21 +0700

    Also change related children

--- SFDynamicHTML.js
+++ SFDynamicHTML.js
@@ -25,37 +25,64 @@ function SFDynamicHTML(html, data){
 // values = {ids:{href, html, src}}
 function SFDynamicHTMLValue(attrName, values){
 	var elemList = document.querySelectorAll('['+attrName+']');
+
+	var getRelatedChildren = function(element){
+		var result = [];
+		var skipExclude = function(parent){
+			var childs = parent.children;
+			for (var i = 0; i < childs.length; i++) {
+				if(childs[i].hasAttribute(attrName)){
+					continue;
+				}
+				else if(childs[i].hasAttribute('sfpart')){
+					result.push(childs[i]);
+				}
+				skipExclude(childs[i]);
+			}
+		}
+		skipExclude(element);
+		return result;
+	}
+
 	for (var i = 0; i < elemList.length; i++){
 		var value = values[elemList[i].getAttribute(attrName)]; // Get the value reference for the ID
 		if(!value) continue; // Skip the unchanged element
 
-		var element = elemList[i];
+		var element = [elemList[i]];
+
+		// Find child element that related to this ID
+		var childs = getRelatedChildren(element[0]);
+		element = element.concat(childs); // Merge it
+
 		for (var a = 0; a < element.length; a++){
 			var sfpart = element[a].getAttribute('sfpart');
 			if(!sfpart) continue; // Dynamic part not found
+			sfpart = sfpart.split(' ').join('').split(',');
 
-			if(sfpart.indexOf('href')!=-1)
-				element[a].setAttribute('href', value.href);
+			for (var z = 0; z < sfpart.length; z++) {
+				if(sfpart[z].indexOf('html')!=-1){
+					var pattern = sfpart[z].split('->'); // ex (*) will be (text)
+					pattern = pattern.length != 1 ? pattern[1] : false;
 
-			if(sfpart.indexOf('html')!=-1){
-				var pattern = sfpart.split('->'); // ex (*) will be (text)
-				pattern = pattern.length != 1 ? pattern[1] : false;
+					element[a].innerHTML = pattern ? pattern.split('*').join(value.html) : value.html;
+				}
 
-				element[a].innerHTML = pattern ? pattern.split('*').join(value.html) : value.html;
-			}
+				else if(sfpart[z].indexOf('src')!=-1){
+					element[a].setAttribute('src', value.src);
 
-			if(sfpart.indexOf('src')!=-1){
-				element[a].setAttribute('src', value.src);
+					// If using Blazy for loading image
+					if(element[a].classList.contains('b-lazy')){
+						element[a].classList.remove('b-error');
+						element[a].setAttribute('data-src', value.src);
+						element[a].setAttribute('src', 'data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=');
+						Blazy();
+					}
+				}
 
-				// If using Blazy for loading image
-				if(element[a].classList.contains('b-lazy')){
-					element[a].classList.remove('b-error');
-					element[a].setAttribute('data-src', value.src);
-					element[a].setAttribute('src', 'data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=');
-					Blazy();
+				else if(value[sfpart[z]]){ // Set attribute only
+					element[a].setAttribute(sfpart[z], value[sfpart[z]]);
 				}
 			}
-
 		}
 	}
 }
